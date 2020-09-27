89ead06f91b2f884409aa2bddda5b45b9134a79a
Daniel Bergey <bergey@simspace.com>
2020-09-26 20:03:51 -0400

    take files to compile on command line, like `ghc --make`

--- app/roll.hs
+++ app/roll.hs
@@ -1,39 +1,51 @@
+{-# LANGUAGE BlockArguments #-}
 module Main where
 
-import           Development.Shake
-import           Development.Shake.Command
-import           Development.Shake.FilePath
-import           Development.Shake.Util
-import           DynFlags
-import           GHC
-import           GHC.Paths                  (libdir)
+import Control.Monad
+import Data.Foldable
+import Data.Maybe
+import Data.Traversable
+import Development.Shake
+import Development.Shake.Command
+import Development.Shake.FilePath
+import Development.Shake.Util
+import DynFlags
+import GHC
+import GHC.Paths (libdir)
+import System.Console.GetOpt as GetOpt
 
-import           Data.Functor
-import           System.Directory
+import Data.Functor
+import System.Directory
 
 main :: IO ()
-main = shakeArgs shakeOptions{shakeFiles="_build"} $ do
-    want ["_build/hello" <.> exe]
+main = shakeArgsWith shakeOptions{shakeFiles=".roll"} rollOptions $ \options targets -> pure $ Just $ do
+    if "clean" `elem` targets then want ["clean"] else want ["all"]
 
-    phony "clean" $ do
-        putInfo "Cleaning files in _build"
-        removeFilesAfter "_build" ["//*"]
+    phony "clean" do
+        putInfo "Cleaning files in .roll"
+        removeFilesAfter ".roll" ["//*"]
 
-    "_build/hello" <.> exe %> \out -> do
-        liftIO $ createDirectoryIfMissing True "_build/objects"
-        need [ "app/roll.hs" ] -- TODO better dependency
-        need [ "hello/Main.hs", "hello/src/Hello/Lib.hs" ]
+    phony "all" do
+        liftIO $ createDirectoryIfMissing True ".roll/objects"
+        let outputFile = foldr (\(ExeFileName file) _ -> Just file) Nothing options
+        when (isJust outputFile) (liftIO $ createDirectoryIfMissing True ".roll/bin")
 
         -- TODO how does shake handle Exceptions?
         -- defaultErrorHandler defaultLogAction $ do
         liftIO $ runGhc (Just libdir) $ do
             dflags <- getSessionDynFlags
             setSessionDynFlags dflags
-                { importPaths = "hello/src" : importPaths dflags
-                , objectDir = Just "_build/objects"
-                , hiDir = Just "_build/objects"
-                , outputFile = Just out
+                { objectDir = Just ".roll/objects"
+                , hiDir = Just ".roll/objects"
+                , outputFile = outputFile
                 }
-            target <- guessTarget "hello/Main.hs" Nothing
-            setTargets [target]
+            setTargets =<< for targets \t -> guessTarget t Nothing
             void $ load LoadAllTargets
+
+rollOptions :: [OptDescr (Either String Options)]
+rollOptions =
+  [ GetOpt.Option "o" [] (ReqArg (Right . ExeFileName . (".roll/bin/" ++)) "FILE")
+    "name of executable"
+  ]
+
+data Options = ExeFileName String
